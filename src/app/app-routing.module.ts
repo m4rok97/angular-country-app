b30c0ff2b2fc943b2b8bc945bdfccc895c3e3a60
Marcos Alderete <m4rok97@gmail.com>
2022-09-18 21:36:25 -0500

    Add routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { ByRegionComponent } from './country/pages/by-region/by-region.component';
+import { ByCountryComponent } from './country/pages/by-country/by-country.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ByCapitalComponent } from './country/pages/by-capital/by-capital.component';
+import { SeeCountryComponent } from './country/pages/see-country/see-country.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ByCountryComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'region',
+    component: ByRegionComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'capital',
+    component: ByCapitalComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'country/:id',
+    component: SeeCountryComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
